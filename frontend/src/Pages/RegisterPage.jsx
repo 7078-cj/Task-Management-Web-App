f156a7ec7ab3b1f05fa4a880fcd71e756ae2d701
Ceejay Santos <7078ceejay@gmail.com>
2025-02-25 22:39:03 +0800

    UI fix log and register

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -26,41 +26,41 @@ function RegisterPage() {
         let data = await response.json()
                 console.log(data)
                 if (response.status ==200){
-                  
+                  loginUser(e)
                   nav('/')
       }
     }
 
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center h-screen'>
       
-      <form onSubmit={RegisterUser} className="form">
+      <form onSubmit={RegisterUser} className="flex flex-col justify-center items-center space-y-2">
         <input 
           type="text" 
           name="username" 
           placeholder="Enter User Name" 
-          className="input"  // Specific class name for username
+          className='outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300' 
         />
         <input 
           type="email" 
           name="email" 
           placeholder="Enter User Email" 
-          className="input-email"  // Specific class name for email
+          className='outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300'  
         />
         <input 
           type="password" 
           name="password" 
           placeholder="Enter Password" 
-          className="input"  // Specific class name for password
+          className='outline-none border-2 rounded-md px-2 py-1 text-slate-500 w-full focus:border-blue-300'  
         />
         <input 
           type="submit" 
           value="Register" 
-          className="input"  // Specific class name for submit button
+          className='w-full justify-center py-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md text-white ring-2'  
         />
     </form>
-    <button className="home-button" onClick={()=>nav('/login')} >Log In</button>
-
+    <h1>Already have an account?? <a href="/login" className='text-cyan-500'>Log in</a></h1>
+ 
       
     </div>
   )
